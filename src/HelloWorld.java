c94243682fe08f98e1557a774e50bc2e12391ad3
Emily Bowersox <emily.bowersox100@gmail.com>
2020-05-01 10:39:44 -0500

    shorthand assignment operators

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,32 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 13;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "3.14159";
+        System.out.println(myString);
+
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+        int x = 4;
+        x += 5;
+        System.out.println(x);
+
+        int z = 3;
+        int y = 4;
+        y *= z;
+        System.out.println(y);
+
+        int a = 10;
+        int b = 2;
+        a /= b;
+        b -= a;
+        System.out.println(a);
+        System.out.println(b);
+
+
     }
 }
