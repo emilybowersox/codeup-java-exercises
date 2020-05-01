4373fd0a94732c2f0e94d69c74f80dd030e664ce
Emily Bowersox <emily.bowersox100@gmail.com>
2020-05-01 14:24:59 -0500

    prompted user for three entries

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -12,7 +12,8 @@ public class ConsoleExercises {
 //        System.out.printf("The value of pi is approximately %.2f%n", pi);
 
 
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
+        //intializing a Scanner variable with the name "scanner"
 
 //        System.out.print("Enter something: ");
 //        String userInput = scanner.nextLine();
@@ -22,9 +23,30 @@ public class ConsoleExercises {
 //        System.out.println("You entered: --> \"" + userInput + "\" <--");
 
 
-        System.out.print("Enter an integer pls: ");
-        int userNumber = scanner.nextInt();
-        System.out.println("You entered the following number: " + userNumber);
+//        System.out.print("Enter an integer pls: ");
+//        int userNumber = scanner.nextInt();
+//        System.out.println("You entered the following number: " + userNumber);
+
+        Scanner words = new Scanner(System.in);
+        //initializing a Scanner variable  the name "words"
+
+        String wordOne;
+        String wordTwo;
+        String wordThree;
+        //initializing variables
+
+        System.out.println("Enter three words. Separate each with a blank or return.");
+        wordOne = words.nextLine();
+        wordTwo = words.nextLine();
+        wordThree = words.nextLine();
+
+        System.out.println("Here are the three words you entered: ");
+        System.out.println(wordOne + " " + wordTwo + " " + wordThree);
+        //if you do it with .nextLine() you have to hit return between each word
+        //-it takes the whole line of an entry as one entry, vs. if you just use
+        //.next() it will divide entries by whitespace
+
+
 
 
     }
