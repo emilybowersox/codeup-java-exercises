6415110d75a3d9c6925419cfda5caf7b4a9375f4
Emily Bowersox <emily.bowersox100@gmail.com>
2020-05-04 12:34:31 -0500

    do-while loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -9,15 +9,29 @@ public class ControlFlowExercises {
 //            i++;
 //        }
 //
+
 //        //do-while loop
-//        int i = -2;
-//        //want it to count from 0-100
-//        //must not be the ideal way to write this
-//
+//        int i = 0;
 //        do {
-//            System.out.println("i = " + (i + 2));
-//            i++;
-//        } while (i <= 98);
+//            System.out.println("i = " + i);
+//            i+=2;
+//        } while (i <= 100);
+
+//        int i = 100;
+//        do {
+//            System.out.println("i = " + i);
+//            i-=5;
+//        } while (i >= -10);
+
+//        int i = 2;
+//        do {
+//            i*=i;
+//            System.out.println("i = " + i);
+//        } while (i <= 65535);
+//        //it breaks at exactly 65,536: which is 256*256
+//        // ^so can't let it calculate it with i = 65536 cause then the answer will be a long
+//        //65,536 squared is like 4 billion which is a long not an int
+//        //??????????????
 
 
 
