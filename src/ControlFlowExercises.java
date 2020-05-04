d6198d27133c25cb86ec14d4e4dd80461fcffe9f
Emily Bowersox <emily.bowersox100@gmail.com>
2020-05-04 12:43:34 -0500

    for loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -23,16 +23,29 @@ public class ControlFlowExercises {
 //            i-=5;
 //        } while (i >= -10);
 
-//        int i = 2;
+//        long i = 2;
 //        do {
-//            i*=i;
 //            System.out.println("i = " + i);
-//        } while (i <= 65535);
+//            i*=i;
+//        } while (i < 1000000L);
 //        //it breaks at exactly 65,536: which is 256*256
 //        // ^so can't let it calculate it with i = 65536 cause then the answer will be a long
 //        //65,536 squared is like 4 billion which is a long not an int
-//        //??????????????
+//      // the solution is just to use long instead of int!!!
+//
+//// refactoring as for-loops
+//        for(int i = 0; i <= 100; i+=2) {
+//            System.out.println("i is " + i);
+//        }
 
+//        for(int i = 100; i >= -10; i-=5) {
+//            System.out.println("i is " + i);
+//        }
+
+//        for(long i = 2; i < 1000000L; i*=i) {
+//            System.out.println("i is " + i);
+//        }
+//        //just have to use long instead of int!!
 
 
 
