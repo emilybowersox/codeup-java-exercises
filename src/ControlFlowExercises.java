95f6d0a2f7832d6b00648fde7b3ae1a99dd17bc0
Emily Bowersox <emily.bowersox100@gmail.com>
2020-05-04 11:37:50 -0500

    while loop

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,18 @@
+public class ControlFlowExercises {
+    public static void main(String[] args){
+
+        //while loop
+        int i = 5;
+
+        while (i <= 15) {
+            System.out.println("i = " + i);
+            i++;
+        }
+
+
+
+
+
+
+    }
+}
