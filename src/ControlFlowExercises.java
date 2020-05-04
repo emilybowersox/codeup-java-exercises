c2f2a5902c3db5613ad528cc2306550e91e51f7e
Emily Bowersox <emily.bowersox100@gmail.com>
2020-05-04 11:52:34 -0500

    do-while loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,13 +1,25 @@
 public class ControlFlowExercises {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        //while loop
-        int i = 5;
+//        //while loop
+//        int i = 5;
+//
+//        while (i <= 15) {
+//            System.out.println("i = " + i);
+//            i++;
+//        }
+//
+//        //do-while loop
+//        int i = -2;
+//        //want it to count from 0-100
+//        //must not be the ideal way to write this
+//
+//        do {
+//            System.out.println("i = " + (i + 2));
+//            i++;
+//        } while (i <= 98);
 
-        while (i <= 15) {
-            System.out.println("i = " + i);
-            i++;
-        }
+
 
 
 
