3d9ffe850fbe36b0dd4753bdca542b7a08870d33
Emily Bowersox <emily.bowersox100@gmail.com>
2020-05-05 10:37:45 -0500

    bob first draft

---
+++ src/Bob.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args){
+
+
+        //Bob answers 'Sure.' if you ask him a question.
+        // (the input ends with a question mark)
+        //He answers 'Whoa, chill out!' if you yell at him.
+        // (the input ends with an exclamation mark)
+        //He says 'Fine. Be that way!'
+        // if you address him without actually saying anything. (empty input)
+        //He answers 'Whatever.' to anything else.
+
+
+
+        Scanner convo = new Scanner(System.in);
+        //make sure you import "import java.util.Scanner;" at top!
+
+        String userInput = convo.nextLine();
+
+        if (userInput.endsWith("?")) {
+            System.out.println("Sure");
+        }
+        else if (userInput.endsWith("!")) {
+            System.out.println("Whoa, chill out!");
+        }
+        else if (userInput.trim().equals("")) {
+            System.out.println("Fine. Be that way!");
+        }
+        else {
+            System.out.println("Whatever.");
+        }
+
+
+
+
+
+    }
+}
