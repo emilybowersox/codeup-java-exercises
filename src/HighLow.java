0ed9a304132f95a3a52cb7729ef22fa28b5364e0
Emily Bowersox <emily.bowersox100@gmail.com>
2020-05-06 10:51:55 -0500

    working on high-low game

---
+++ src/HighLow.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class HighLow {
+    public static void main(String[] args) {
+
+//generate random  number between 1- 100
+    // java.lang.Math
+
+    Scanner game = new Scanner(System.in);
+    System.out.println("please guess a number between 1 and 100: ");
+    int userGuess = game.nextInt();
+//    System.out.println(userGuess);
+
+
+
+
+
+
+
+
+}
+}
--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,40 +1,69 @@
-public class MethodsExercises {
+import java.util.Scanner;
 
-    public static void add(int a, int b) {
-        System.out.println(a+b);
-    }
+//public class MethodsExercises {
 
-    public static void subtract(int a, int b){
-        System.out.println(a-b);
-    }
-
-    public static void multiply(int a, int b){
-        System.out.println(a*b);
-    }
-
-    public static void divide(double a, double b){
-        System.out.println(a/b);
-    }
-    //if you say int instead of double it's going to round it
-
-
-
-
-
-
-
-    public static void main(String[] args) {
-        add(5, 10);
-        subtract(4,2);
-        multiply(4,7);
-        divide(3,2);
-    }
-
-
-
+//    public static void add(int a, int b) {
+//        System.out.println(a+b);
+//    }
+//
+//    public static void subtract(int a, int b){
+//        System.out.println(a-b);
+//    }
+//
+//    public static void multiply(int a, int b){
+//        System.out.println(a*b);
+//    }
+//
+//    public static void divide(double a, double b){
+//        System.out.println(a/b);
+//    }
+//    //if you say int instead of double it's going to round it
+//
+//    public static void modulus(int a, int b){
+//        System.out.println(a%b);
+//    }
+//
+//    public static void main(String[] args) {
+//        add(5, 10);
+//        subtract(4,2);
+//        multiply(4,7);
+//        divide(3,2);
+//        modulus(5,2);
+//    }
 
 
-}
+//2
+//    Scanner aNumber = new Scanner(System.in);
+//
+//    System.out.print("Enter a number between 1 and 10: ");
+////    int userInput = getInteger(1, 10);
+//    int userInput = aNumber.nextInt();
+//
+//    public static int getInteger(int min, int max){
+//        if (min >= 1 && max <= 10) {
+//            System.out.println("You entered a valid age!");
+//            return;
+//        }
+//        System.out.println("Please try again.");
+//    }
+//
+//    public static void main(String[] args){
+//        getInteger(userInput);
+//
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+//}
 
 
 
