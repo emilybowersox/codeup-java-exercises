a7254e2d665bb3ac509ca9b24a458edb9d0ee19e
Emily Bowersox <emily.bowersox100@gmail.com>
2020-05-06 12:04:39 -0500

    get integer

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,22 +1,33 @@
-import java.util.Scanner;
-
-public class HighLow {
-    public static void main(String[] args) {
-
-//generate random  number between 1- 100
-    // java.lang.Math
-
-    Scanner game = new Scanner(System.in);
-    System.out.println("please guess a number between 1 and 100: ");
-    int userGuess = game.nextInt();
-//    System.out.println(userGuess);
-
-
-
-
-
-
-
-
-}
-}
+//import java.util.Scanner;
+//import java.util.Random;
+//
+//public class HighLow {
+//    public static void main(String[] args) {
+//
+////generate random  number between 1- 100
+//    // java.lang.Math
+//
+//        int randomNumber =
+//                Random randomNumber = new Random();
+//        int randomNumber = rand.
+//                Scanner game = new Scanner(System.in);
+//    System.out.println("please guess a number between 1 and 100: ");
+//    int userGuess = game.nextInt();
+////    System.out.println(userGuess);
+//
+//
+//
+//        if ((userGuess > randomNumber) && (userGuess <= 100))
+//            System.out.println("LOWER");
+//        else if ((userGuess < randomNumber) && (userGuess >= 1))
+//            System.out.println("HIGHER");
+//        else if (userGuess == randomNumber)
+//            System.out.println("GOOD GUESS, CONGRATS!");
+//
+//
+//
+//
+//
+//
+//}
+//}
--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,69 +1,66 @@
 import java.util.Scanner;
 
-//public class MethodsExercises {
+public class MethodsExercises {
 
-//    public static void add(int a, int b) {
-//        System.out.println(a+b);
+//    public static int add(int a, int b) {
+//       return a + b;
 //    }
 //
-//    public static void subtract(int a, int b){
-//        System.out.println(a-b);
+//    public static int subtract(int a, int b){
+//        return a-b;
 //    }
 //
 //    public static void multiply(int a, int b){
 //        System.out.println(a*b);
 //    }
 //
-//    public static void divide(double a, double b){
-//        System.out.println(a/b);
+//    public static double divide(double a, double b){
+//      if (b == 0){
+//            return 0;
+//                    }
+//      //division by zero equals infinity
+//        return a/b;
 //    }
 //    //if you say int instead of double it's going to round it
 //
-//    public static void modulus(int a, int b){
-//        System.out.println(a%b);
+//    public static int modulus(int a, int b){
+//        return a%b;
 //    }
 //
-//    public static void main(String[] args) {
-//        add(5, 10);
-//        subtract(4,2);
+    public static void main(String[] args) {
+//        System.out.println(add(5, 10));
+//        System.out.println(subtract(4,2));
 //        multiply(4,7);
-//        divide(3,2);
-//        modulus(5,2);
-//    }
-
-
-//2
-//    Scanner aNumber = new Scanner(System.in);
-//
-//    System.out.print("Enter a number between 1 and 10: ");
-////    int userInput = getInteger(1, 10);
-//    int userInput = aNumber.nextInt();
-//
-//    public static int getInteger(int min, int max){
-//        if (min >= 1 && max <= 10) {
-//            System.out.println("You entered a valid age!");
-//            return;
-//        }
-//        System.out.println("Please try again.");
-//    }
-//
-//    public static void main(String[] args){
-//        getInteger(userInput);
-//
-//    }
+//        System.out.println(divide(3,2));
+//        System.out.println(modulus(5,2));
+            getInteger(1, 10);
+    }
 
 
 
 
 
 
+    public static int getInteger(int min, int max) {
+        Scanner aNumber = new Scanner(System.in);
+        System.out.println("Enter a number between 1 and 10: ");
+//    int userInput = getInteger(1, 10);
+        int userInput = Integer.parseInt(aNumber.nextLine());
+
+        //how to proceed
+        if (userInput < min || userInput > max) {
+            System.out.println("Not a valid number");
+            return getInteger(min, max);
+        } else {
+            //how to stop
+            System.out.println("Number is within range");
+            return userInput;
+        }
 
 
+    }
 
-
-
-
-//}
+}
 
 
 
