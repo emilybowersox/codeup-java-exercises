44473762122b83b16ed2cfe9065476ad9b7b7222
Emily Bowersox <emily.bowersox100@gmail.com>
2020-05-11 15:18:11 -0500

    shapes test

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -1,4 +1,34 @@
 package shapes;
 
+//Test your code by creating a new class, ShapesTest (also inside of shapes) with a main method.
+// Inside the main method,
+
+// create a variable of the type Rectangle named box1
+// and assign it a new instance of the Rectangle class with a width of 4 and a length of 5
+//
+// verify that the getPerimeter and getArea methods return 18 and 20, respectively.
+//
+// create a variable of the type Rectangle named box2 and assign it a new instance of the Square class that has a side value of 5.
+//
+// verify that the getPerimeter and getArea methods return 20 and 25, respectively.
+
+
+
 public class ShapesTest {
+
+public static void main(String[] args){
+
+Rectangle box1 = new Rectangle(4, 5);
+
+    System.out.println("the perimeter is " + box1.getPerimeter());
+    System.out.println("the area is " + box1.getArea());
+
+
+}
+
+
+
+
+
+
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,20 +1,20 @@
 package shapes;
 
-public class Square extends Rectangle {
-    private int side;
-    public Square(int side) {
-        super(aLength, aWidth);
-        this.side = side;
-    }
-
-    public int getArea() {
-        return
-    }
-
-//    public Square(int aLength, int aWidth) {
+//public class Square extends Rectangle {
+//    private int side;
+//    public Square(int side) {
 //        super(aLength, aWidth);
+//        this.side = side;
 //    }
-}
+//
+//    public int getArea() {
+//        return
+//    }
+//
+////    public Square(int aLength, int aWidth) {
+////        super(aLength, aWidth);
+////    }
+//}
 
 
 //Create a class named Square, also inside of shapes, that extends Rectangle.
