19cd7aef74a01b58920d03791eca406b387d2320
Emily Bowersox <emily.bowersox100@gmail.com>
2020-05-11 14:32:02 -0500

    square

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,4 +1,23 @@
 package shapes;
 
-public class Square {
+public class Square extends Rectangle {
+    private int side;
+    public Square(int side) {
+        super(aLength, aWidth);
+        this.side = side;
+    }
+
+    public int getArea() {
+        return
+    }
+
+//    public Square(int aLength, int aWidth) {
+//        super(aLength, aWidth);
+//    }
 }
+
+
+//Create a class named Square, also inside of shapes, that extends Rectangle.
+// Square should define a constructor that accepts one argument, side,
+// and calls the parent's constructor to set both the length and width
+// to the value of side.
