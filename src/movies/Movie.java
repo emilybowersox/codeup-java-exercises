67b8962bc836a3483962c7674b5b78e1df75586c
Emily Bowersox <emily.bowersox100@gmail.com>
2020-05-24 21:56:35 -0500

    movie constructor, getters and setters

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -1,4 +1,40 @@
 package movies;
 
 public class Movie {
+
+   //declaring the variables
+    private String name;
+    private String category;
+
+
+    //constructor
+    public Movie(String movieName, String movieCategory) {
+        this.name = movieName;
+        this.category = movieCategory;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setName(String newName){
+        this.name = newName;
+    }
+
+    public void setCategory(String newCategory){
+        this.category = newCategory;
+    }
+
+
+
+
+
+
+
+
 }
